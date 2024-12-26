500e0785d32511513000e08ccac0abbe3c8f6780
老阿張 <laoazhang@163.com>
2024-12-26 23:45:06 +0800

    add Swagger、Knife4j、Yapi

--- stock_backend/src/main/java/cn/laoazhang/stock/controller/UserController.java
+++ stock_backend/src/main/java/cn/laoazhang/stock/controller/UserController.java
@@ -5,6 +5,8 @@ import cn.laoazhang.stock.service.UserService;
 import cn.laoazhang.stock.vo.req.LoginReqVo;
 import cn.laoazhang.stock.vo.resp.LoginRespVo;
 import cn.laoazhang.stock.vo.resp.R;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/api")
+@Api(value = "用户认证相关功能接口定义",tags = "用户功能 - 用户登录功能")
 public class UserController {
 
     @Autowired
@@ -28,6 +31,7 @@ public class UserController {
      * @return
      */
     @GetMapping("/{userName}")
+    @ApiOperation(value = "根据用户名查询用户信息",notes = "用户信息查询",response = SysUser.class)
     public SysUser getUserService(@PathVariable("userName") String userName) {
         return userService.getUserByUserName(userName);
     }
@@ -39,6 +43,7 @@ public class UserController {
      * @return
      */
     @PostMapping("/login")
+    @ApiOperation(value = "用户登录功能",notes = "用户登录",response = R.class)
     public R<LoginRespVo> login(@RequestBody LoginReqVo vo) {
         R<LoginRespVo> r = userService.login(vo);
         return r;
@@ -49,6 +54,7 @@ public class UserController {
      * @return
      */
     @GetMapping("/captcha")
+    @ApiOperation(value = "验证码生成功能",response = R.class)
     public R<Map> getCaptchaCode() {
         return userService.getCaptchaCode();
     }
